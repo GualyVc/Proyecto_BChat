88c8f5dd0e6e29216e1c0873abb7728e0b62c743
GualyVc <gualy_vc@hotmail.com>
2015-04-03 12:08:54 -0400

    Primer activity con login

---
+++ app/src/main/java/com/example/gualy/bchat/Fragment_Login.java
@@ -0,0 +1,38 @@
+package com.example.gualy.bchat;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by GUALY on 02/04/2015.
+ */
+public class Fragment_Login extends Fragment {
+    private Button login_boton_ingresar;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view;
+
+        view = inflater.inflate(R.layout.fragment_login,container);
+
+        login_boton_ingresar = (Button)view.findViewById(R.id.login_button_ingresar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (Fragment_Login.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
+    }
+
+
+}
