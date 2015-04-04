bf206fb62c89cbb2fcafff23a5481ce0242ee905
GualyVc <gualy_vc@hotmail.com>
2015-04-04 13:11:18 -0400

    Login con fragment e intent y base de datos2

--- app/src/main/java/com/example/gualy/bchat/Db_Users.java
+++ app/src/main/java/com/example/gualy/bchat/Db_Users.java
@@ -1,5 +1,6 @@
 package com.example.gualy.bchat;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,6 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by GUALY on 04/04/2015.
  */
 public class Db_Users extends SQLiteOpenHelper {
+
     private static final String DB_NAME = "usuarios";
     private static final int SCHEME_VERSION = 1;
     private SQLiteDatabase db;
@@ -15,6 +17,20 @@ public class Db_Users extends SQLiteOpenHelper {
     public Db_Users (Context context){
         super(context, DB_NAME, null, SCHEME_VERSION);
         db = this.getWritableDatabase();
+    }
+
+    private ContentValues generarValores(Tabla usuarios){
+        ContentValues valores = new ContentValues();
+        valores.put(Tabla.FIELD_email,usuarios.getEmail());
+        valores.put(Tabla.FIELD_password,usuarios.getPassword());
+        return valores;
+
+    }
+
+    public void insertarUsuarios(Tabla usuarios)
+    {
+        db.insert(Tabla.TABLE_NAME,null,generarValores(usuarios));
+
     }
 
     @Override
